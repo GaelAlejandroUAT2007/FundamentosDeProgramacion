958bf40098a0dc9af2286797ebe15a6dee8354b5
GaelAlejandroUAT2007 <a2253332351@alumnos.uat.edu.mx>
2026-09-14 19:10:24 -0600

    Add files via upload

---
+++ Actividades_en_Clase/Unidad2/a2253332351_practica07/a2253332351_practica07/ejercicio1_CD.java
@@ -0,0 +1,21 @@
+package a2253332351_practica07;
+
+import javax.swing.JOptionPane;
+
+public class ejercicio1_CD {
+	public static void main(String args[]) {
+		int num;
+		String entrada;
+		String salida;
+		entrada = JOptionPane.showInputDialog("Introduce un Numero");
+		num = Integer.parseInt(entrada);
+		salida = "";
+		if (num > 0)
+			salida = "Es positivo";
+		else
+			salida = "Es Negativo";
+		JOptionPane.showMessageDialog(null, salida);
+		System.exit(0);
+	}
+}
+
